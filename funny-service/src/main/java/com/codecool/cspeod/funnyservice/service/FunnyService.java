37668043717348ce15997731350492cd49bafc67
csepdo <arodzeta@gmail.com>
2019-07-03 10:20:33 +0200

    Created base structure for FunnyService

---
+++ funny-service/src/main/java/com/codecool/cspeod/funnyservice/service/FunnyService.java
@@ -0,0 +1,9 @@
+package com.codecool.cspeod.funnyservice.service;
+
+import com.codecool.cspeod.funnyservice.model.Joke;
+
+public class FunnyService {
+    public static Joke newJoke() {
+        return null;
+    }
+}
