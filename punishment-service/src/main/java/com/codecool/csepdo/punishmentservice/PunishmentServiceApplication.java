5101a3deea335a388a38a5f72f4d4bbaa3442dff
csepdo <arodzeta@gmail.com>
2019-07-03 18:00:03 +0200

    Integrated under Eureka

--- punishment-service/src/main/java/com/codecool/csepdo/punishmentservice/PunishmentServiceApplication.java
+++ punishment-service/src/main/java/com/codecool/csepdo/punishmentservice/PunishmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.codecool.csepdo.punishmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PunishmentServiceApplication {
 
     public static void main(String[] args) {
