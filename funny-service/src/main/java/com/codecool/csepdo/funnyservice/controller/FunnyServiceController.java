08c51ab2728e21f4197c585f3e76009f81f6e902
csepdo <arodzeta@gmail.com>
2019-07-03 15:27:21 +0200

    Change return type to String

--- funny-service/src/main/java/com/codecool/csepdo/funnyservice/controller/FunnyServiceController.java
+++ funny-service/src/main/java/com/codecool/csepdo/funnyservice/controller/FunnyServiceController.java
@@ -1,17 +1,25 @@
 package com.codecool.csepdo.funnyservice.controller;
 
-import com.codecool.csepdo.funnyservice.model.Joke;
 import com.codecool.csepdo.funnyservice.service.FunnyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("funny-service")
+@RequestMapping("/funny-service")
 public class FunnyServiceController {
 
+    @Autowired
+    private FunnyService funnyService;
+
     @GetMapping("/new-joke")
-    public Joke getNewJoke() {
-        return FunnyService.newJoke();
+    public String getNewJoke() {
+        String newImage = funnyService.newImage();
+        if (newImage != null) {
+            return newImage;
+        } else {
+            return "Sorry, no funny images for today.";
+        }
     }
 }
