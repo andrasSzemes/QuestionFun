d0ea0e294bc7ec87774732a3176d2334ab6aa53f
csepdo <arodzeta@gmail.com>
2019-07-03 15:50:42 +0200

    Created base punishment-service module

---
+++ punishment-service/src/main/java/com/codecool/csepdo/punishmentservice/PunishmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.csepdo.punishmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunishmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PunishmentServiceApplication.class, args);
+    }
+
+}
