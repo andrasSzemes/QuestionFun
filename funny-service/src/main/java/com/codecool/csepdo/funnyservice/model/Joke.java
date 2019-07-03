87d334ed901c4e186dfcb2c1faff24a54ccb6aed
csepdo <arodzeta@gmail.com>
2019-07-03 10:41:48 +0200

    Added necessary fields and Lombok

--- funny-service/src/main/java/com/codecool/csepdo/funnyservice/model/Joke.java
+++ funny-service/src/main/java/com/codecool/csepdo/funnyservice/model/Joke.java
@@ -1,4 +1,16 @@
 package com.codecool.csepdo.funnyservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Joke {
+
+    private String category;
+    private String type;
+    private String joke;
+    private String id;
 }
