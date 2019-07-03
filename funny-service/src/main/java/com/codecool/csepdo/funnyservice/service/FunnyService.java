ba7b61c54e1000e0b2ab90e2be6599c47fe4c619
csepdo <arodzeta@gmail.com>
2019-07-03 15:26:46 +0200

    Change from API call to reading from file

--- funny-service/src/main/java/com/codecool/csepdo/funnyservice/service/FunnyService.java
+++ funny-service/src/main/java/com/codecool/csepdo/funnyservice/service/FunnyService.java
@@ -1,9 +1,35 @@
 package com.codecool.csepdo.funnyservice.service;
 
-import com.codecool.csepdo.funnyservice.model.Joke;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
 public class FunnyService {
-    public static Joke newJoke() {
-        return null;
+
+    @Value("${filepath.url}")
+    private String filepath;
+
+    private List<String> urls = new ArrayList<>();
+
+    private Random random = new Random();
+
+    public String newImage() {
+        readFile();
+        return urls.get(random.nextInt(urls.size()));
+    }
+
+    public void readFile() {
+        try {
+            urls = Files.readAllLines(Paths.get("funny-service/src/main/resources/links.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
