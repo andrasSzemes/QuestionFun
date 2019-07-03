eb4bcc2da2a88fb5850fcfd081941858b9367f76
csepdo <arodzeta@gmail.com>
2019-07-03 10:28:59 +0200

    Test directory created

---
+++ cat-service/src/test/java/com/codecool/csepdo/catservice/CicaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.csepdo.catservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicaServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
