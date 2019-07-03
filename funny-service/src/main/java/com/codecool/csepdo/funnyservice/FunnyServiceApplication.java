5d3c91fd1322ca9f7e38f80281bddebbeeabc57d
csepdo <arodzeta@gmail.com>
2019-07-03 17:59:36 +0200

    Integrated under Eureka

--- funny-service/src/main/java/com/codecool/csepdo/funnyservice/FunnyServiceApplication.java
+++ funny-service/src/main/java/com/codecool/csepdo/funnyservice/FunnyServiceApplication.java
@@ -2,8 +2,10 @@ package com.codecool.csepdo.funnyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FunnyServiceApplication {
 
     public static void main(String[] args) {
