e4ac0477451e3d09c12dd1973a88014e67276a88
Csaba Szabó <szabcsabest@gmail.com>
2019-07-04 15:29:08 +0200

    videos are served encoded to base64

--- punishment-service/src/main/java/com/codecool/csepdo/punishmentservice/service/PunishmentService.java
+++ punishment-service/src/main/java/com/codecool/csepdo/punishmentservice/service/PunishmentService.java
@@ -4,10 +4,14 @@ import com.codecool.csepdo.punishmentservice.model.Punishment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Random;
 
@@ -23,7 +27,20 @@ public class PunishmentService {
 
     public Punishment newVideo() {
         readFile();
-        return new Punishment(urls.get(random.nextInt(urls.size())));
+        String path = urls.get(random.nextInt(urls.size()));
+        File video = new File(path);
+        byte[] videoBytes = new byte[(int) video.length()];
+        try {
+            FileInputStream videoByteStream = new FileInputStream(video);
+            videoByteStream.read(videoBytes);
+            videoByteStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String base64Video = Base64.getEncoder().withoutPadding().encodeToString(videoBytes);
+
+        return new Punishment("data:video/mp4;base64," + base64Video);
     }
 
     public void readFile() {
