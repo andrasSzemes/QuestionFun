a45d843c40369e3a768b4304196feb57ccaa96f0
csepdo <arodzeta@gmail.com>
2019-07-04 16:14:18 +0200

    Removed unused imports

--- punishment-service/src/main/java/com/codecool/csepdo/punishmentservice/service/PunishmentService.java
+++ punishment-service/src/main/java/com/codecool/csepdo/punishmentservice/service/PunishmentService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
