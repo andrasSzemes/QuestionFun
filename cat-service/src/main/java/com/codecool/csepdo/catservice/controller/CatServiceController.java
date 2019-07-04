2eb091826d09d6a4fe09452f749b3add346b8970
csepdo <arodzeta@gmail.com>
2019-07-04 16:12:44 +0200

    Removed unused imports

--- cat-service/src/main/java/com/codecool/csepdo/catservice/controller/CatServiceController.java
+++ cat-service/src/main/java/com/codecool/csepdo/catservice/controller/CatServiceController.java
@@ -3,7 +3,6 @@ package com.codecool.csepdo.catservice.controller;
 import com.codecool.csepdo.catservice.model.CatReward;
 import com.codecool.csepdo.catservice.service.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
